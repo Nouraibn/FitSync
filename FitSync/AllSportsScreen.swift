65573c8a47079209ba2619dfacd8837f77195d31
mahaanaif <mahaa.alharbi76@gmail,com>
2024-09-29 17:06:05 +0300

    add

---
+++ FitSync/AllSportsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AllSportsScreen.swift
+//  FitSync
+//
+//  Created by MAHA on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct AllSportsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllSportsScreen()
+}
